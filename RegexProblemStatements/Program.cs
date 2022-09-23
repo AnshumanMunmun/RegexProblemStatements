5bf55d65cab53824eace98458440c7f9c0f4f9a4
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-23 22:09:39 +0530

    [Anshuman] Add: Validating Last name

--- RegexProblemStatements/Program.cs
+++ RegexProblemStatements/Program.cs
@@ -5,7 +5,24 @@
         static void Main(string[] args)
         {
             UserValidation userValidation = new UserValidation();
-            userValidation.FirstNameValidation();
+            Console.WriteLine("User Registration Form ");
+            while (true)
+            {
+                Console.WriteLine("Please choose the option :\n1. First Name \n2. Last Name");
+                int option = Convert.ToInt16(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        userValidation.FirstNameValidation();
+                        break;
+                    case 2:
+                        userValidation.LastNameValidation();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter the correct value.");
+                        break;
+                }
+            }
         }
     }
 }
--- RegexProblemStatements/UserValidation.cs
+++ RegexProblemStatements/UserValidation.cs
@@ -13,12 +13,27 @@ namespace RegexProblemStatements
         {
             Console.WriteLine("Enter First Name");
             string Firstname = Console.ReadLine();
-            string firstname = "^[A-Z]{1}[a-z]{2}";
+            string firstname = "^[A-Z]{1}[a-z]{2,}$";
             Regex regex = new Regex(firstname);
 
             if (regex.IsMatch(Firstname))
             {
                 Console.Write("Name " + Firstname +  " is Valid \n");
+            }
+            else
+            {
+                Console.Write("Please Enter First Letter Capital \n");
+            }
+        }
+        public void LastNameValidation()
+        {
+            Console.WriteLine("Enter Last Name");
+            string Lastname = Console.ReadLine();
+            string lastname = "^[A-Z]{1}[a-z]{2,}$";
+            Regex regex = new Regex(lastname);
+            if (regex.IsMatch(Lastname))
+            {
+                Console.Write("Name " + Lastname + " is Valid \n");
             }
             else
             {
